f36a3d429228f213b43d5f9dfe1279e9961aceb8
FonsahPageo <ashprincepageo@gmail.com>
2024-07-01 16:14:35 +0100

    playing one round

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function getHumanChoice() {
 
 console.log("You chose ", getHumanChoice());
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        alert("It's a tie. Play again")
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice == "rock") ||
+        (humanChoice === "sciccors" && computerChoice === "rock")
+    ) {
+        humanScore += 1;
+    } else {
+        computerScore += 1;
+    }
+}
+
